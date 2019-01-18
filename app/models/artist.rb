6d3e72f3c569c1f0d5e463e00cab57521605deae
Stephanie Abud <sabud1208@gmail.com>
2019-01-18 16:25:02 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,4 @@
 class Artist < ActiveRecord::Base
+  has_many :song 
+  has_many :genres, through: :songs
 end
--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,2 +1,3 @@
 class AddArtistToSongs < ActiveRecord::Migration[4.2]
+  has_many :song
 end
